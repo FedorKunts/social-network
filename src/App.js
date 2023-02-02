6648e44c98da7961b809778cea2bb2aba6392856
FedorKunts <fedkunts@yandex.ru>
2023-02-02 12:15:30 +0700

    test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='app-wrapper'>
+      <header className='header'>
+        <img src=''></img>
       </header>
+      <nav className='nav'>
+        <div>
+          profile
+        </div>
+      </nav>
+      <div className='content'>
+        content
+      </div>
+      <footer className='footer'>
+        fff
+      </footer>
+      
     </div>
   );
 }
