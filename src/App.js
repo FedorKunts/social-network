80a645faabf3bbe05e8ae69cf9d4f87539450001
FedorKunts <fedkunts@yandex.ru>
2023-02-02 14:45:22 +0700

    test2

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ const App = () => {
   return (
     <div className='app-wrapper'>
       <header className='header'>
-        <img src=''></img>
+        <img src='https://img.freepik.com/premium-vector/initials-m-logo-design-isolated-vector-illustration_595532-436.jpg?w=826' />
       </header>
       <nav className='nav'>
         <div>
